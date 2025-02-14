4b602404d18f6d0abd97374945276f5f4b9379c8
dimkastaniotis <dim.kastaniotis@gmail.com>
2025-02-14 18:42:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,47 +6,41 @@ import os
 app = Flask(__name__)
 
 # Ρύθμιση Βάσης Δεδομένων (SQLite)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db' #Σχετική διαδρομή
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Μοντέλο TODO
-class Todo(db.Model):
+# Μοντέλο Τραγουδιού
+class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
+    title = db.Column(db.String(200))
+    artist = db.Column(db.String(200))
 
 # Routes
 @app.route('/')
 def index():
-    todos = Todo.query.all()
-    return render_template('index.html', todos=todos)
+    songs = Song.query.all()
+    return render_template('index.html', songs=songs)
 
 @app.route('/add', methods=['POST'])
 def add():
-    text = request.form['text']
-    todo = Todo(text=text, complete=False)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/complete/<id>')
-def complete(id):
-    todo = Todo.query.get(int(id))
-    todo.complete = True
+    title = request.form['title']
+    artist = request.form['artist']
+    song = Song(title=title, artist=artist)
+    db.session.add(song)
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/delete/<id>')
 def delete(id):
-    todo = Todo.query.get(int(id))
-    db.session.delete(todo)
+    song = Song.query.get(int(id))
+    db.session.delete(song)
     db.session.commit()
     return redirect(url_for('index'))
 
 #Δημιουργία των πινάκων
 with app.app_context():
-        db.create_all()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
